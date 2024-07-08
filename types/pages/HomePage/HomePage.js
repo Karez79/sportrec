f2dc4e2a184165a52121638a0ac43e184cdfea29
Karez79 <Karez79@users.noreply.github.com>
2024-07-08 14:43:43 +0000

    deploy: 9e05fe76feaa356c8d44be908ca04d5f9f5be814

---
+++ types/pages/HomePage/HomePage.js
@@ -0,0 +1,8 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import ImageSlider from '../../components/Slyder/Slider';
+import Columns from '../../components/Columns/Columns';
+import './HomePage.css';
+const HomePage = () => {
+    return (_jsxs("div", { className: "home-page", children: [_jsx(ImageSlider, {}), _jsx(Columns, {})] }));
+};
+export default HomePage;
