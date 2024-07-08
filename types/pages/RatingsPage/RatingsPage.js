33466785bed3ff8b18612936a567a39069902201
Ivan <karez79@yandex.ru>
2024-07-09 00:19:39 +0200

    Updates

--- types/pages/RatingsPage/RatingsPage.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import './RatingsPage.css';
-const RatingsPage = () => {
-    return (_jsx("div", { className: "ratings-page", children: _jsx("h1", { children: "Ratings Page" }) }));
-};
-export default RatingsPage;
