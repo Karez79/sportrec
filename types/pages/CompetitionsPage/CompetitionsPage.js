10e7b6617c136873c6f213bcc61a3c82a5458281
Ivan <karez79@yandex.ru>
2024-07-08 17:41:12 +0200

    Updates

--- types/pages/CompetitionsPage/CompetitionsPage.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import './CompetitionsPage.css';
-const CompetitionsPage = () => {
-    return (_jsx("div", { className: "competitions-page", children: _jsx("h1", { children: "Competitions Page" }) }));
-};
-export default CompetitionsPage;
